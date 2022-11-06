7c933c701a104c710aeeed3c6cb43dee92649afe
Chanhun Jeong <58506361+jch5158@users.noreply.github.com>
2022-11-07 01:21:42 +0900

    .

--- js/background.js
+++ js/background.js
@@ -1,13 +1,11 @@
-const images = [
-    "0.jpg",
-    "1.jpg",
-    "2.jpg"
-];
-
+const images = ["0.jpg", "1.jpg", "2.jpg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
+//const bgImage = document.createElement("div");
 
-const bgImage = document.createElement("img");
+const body = document.querySelector("body");
 
-bgImage.src = `img/${chosenImage}`;
+body.style.backgroundImage = `url(./img/${chosenImage})`;
 
-document.body.appendChild(bgImage);
+body.classList.add("bgImg");
+
+//document.body.appendChild(bgImage);
--- js/greetings.js
+++ js/greetings.js
@@ -1,28 +1,35 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const nameQuery = document.querySelector("#name-query");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
-const USERNAME_KEY = "username"
+const USERNAME_KEY = "username";
 
-function onLoginSubmit(eventInfo){
-    eventInfo.preventDefault();
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-    localStorage.setItem(USERNAME_KEY, loginInput.value);
-    paintGreetings();
+function onLoginSubmit(eventInfo) {
+  eventInfo.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  nameQuery.classList.add(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY, loginInput.value);
+  paintGreetings();
 }
 
-function paintGreetings(){
-    const username = localStorage.getItem(USERNAME_KEY);
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+function paintGreetings() {
+  const username = localStorage.getItem(USERNAME_KEY);
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  document.querySelector("#clock").classList.remove(HIDDEN_CLASSNAME);
+  document.querySelector("#todo-form").classList.remove(HIDDEN_CLASSNAME);
+  document.querySelector("#todo-list").classList.remove(HIDDEN_CLASSNAME);
+  document.querySelector("#quote").classList.remove(HIDDEN_CLASSNAME);
+  document.querySelector("#todo-text").classList.remove(HIDDEN_CLASSNAME);
+  nameQuery.classList.add(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
-
-if(savedUsername === null){
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener("submit",onLoginSubmit);
-}else{
-    paintGreetings();
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings();
 }
